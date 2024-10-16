22db70611e23d5b68b51eef1dbbc1575bf8a14a4
Skeltex <dmtr.afanasyev@gmail.com>
2024-10-16 10:00:26 +0400

    add pro0 homework

---
+++ homework/pro0/A/A/A.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<vector <int>> g;
+vector<bool> used;
+
+
+void dfs(int v)
+{
+    used[v - 1] = true;
+    // Запускаем dfs от всех вершин, связанных с этой, в которых мы не были
+    for (int i = 0; i < g[v - 1].size(); ++i)
+        if (!used[g[v - 1][i] - 1])
+            dfs(g[v - 1][i]);
+}
+
+
+int main()
+{
+    // Ввод графа
+    int n, m, v, u;
+    cin >> n >> m;
+    g.resize(n);
+    used.resize(n);
+    
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> v >> u;
+        g[v - 1].push_back(u);
+        g[u - 1].push_back(v);
+    }
+    
+
+    /*
+    // Вывод списка смежности
+    cout << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ": ";
+        for (int j = 0; j < g[i].size(); ++j)
+            cout << g[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+    */
+
+
+    if (n - m != 1)
+        cout << "NO";
+    else
+    {
+        // Проверка на связность - что из вершины с номером 1 можно дойти до всех
+        dfs(1);
+        
+
+        /*
+        // Вывод списка посещённости
+        cout << endl;
+        for (int i = 0; i < n; ++i)
+        {
+            cout << i + 1 << ": " << used[i] << endl;
+        }
+        cout << endl;
+        */
+
+
+        
+        // Если хотя бы 1 вершина осталась непосещённой - несвязный - не дерево
+        bool flag = true;
+        for (int i = 0; i < n; ++i)
+        {
+            if (!used[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+        cout << (flag? "YES" : "NO");
+    }
+
+
+    return 0;
+}
---
+++ homework/pro0/B/B/B.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector <short> parents;
+vector <short> colors;
+vector<vector <short>> tree;
+
+vector <short> result;
+
+
+int dfs(short v, short t1)
+{
+    // Считаем сумму по цветам
+    short sum = colors[v - 1];
+    for (short i = 0; i < tree[v - 1].size(); ++i)
+        sum += dfs(tree[v - 1][i], t1);
+
+    if (sum == 0) // Если баланс по цветам
+        result[t1] += 1;
+    
+    return sum;
+}
+
+
+int main()
+{
+    short t, n;
+    string str_colors;
+
+    cin >> t;
+    result.resize(t);
+
+    for (short t1 = 0; t1 < t; ++t1)
+    {
+        cin >> n;
+        parents.clear();
+        parents.resize(n - 1);
+        colors.clear();
+        colors.resize(n);
+        tree.clear();
+        tree.resize(n);
+
+        // Ввод данных
+        for (short i = 0; i < n - 1; ++i)
+            cin >> parents[i];
+
+        cin >> str_colors;
+        for (short i = 0; i < n; ++i)
+            colors[i] = ((str_colors[i] == 'W') ? -1 : 1);
+
+
+        
+
+        // Создание массива смежности (без предков)
+        for (short i = 0; i < n - 1; ++i)
+        {
+            // для parents[i] потомок i + 2
+            tree[parents[i] - 1].push_back(i + 2);
+        }
+
+
+
+        /*
+        // Вывод массива смежности
+        cout << endl;
+        for (short i = 0; i < n; ++i)
+        {
+            cout << i + 1 << ": ";
+            for (short j = 0; j < tree[i].size(); j++)
+            {
+                cout << tree[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        */
+
+        
+
+        dfs(1, t1);
+    }
+
+    for (short t1 = 0; t1 < t; ++t1)
+        cout << result[t1] << endl;
+
+    return 0;
+}
